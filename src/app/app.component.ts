e84a30d510c3c20923c0238e8528a07c3c6def94
Antony Lucas <54651622+Antony-Lucas@users.noreply.github.com>
2023-12-12 08:54:53 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { EventBusService } from './_shared/event.bus.service';
+import { StorageService } from './_services/storage.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'knowbitests';
+export class AppComponent implements OnInit, OnDestroy {
+  private roles: string[] = [];
+  isLoggedIn = false;
+  showAdminBoard = false;
+  showModeratorBoard = false;
+  username?: string;
+
+  eventBusSub?: Subscription;
+
+  constructor(
+    private storageService: StorageService,
+    private eventBusService: EventBusService
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoggedIn = !!this.storageService.getToken();
+
+    if (this.isLoggedIn) {
+      const user = this.storageService.getUser();
+      this.roles = user.roles;
+
+      this.showAdminBoard = this.roles.includes('ROLE_ADMIN');
+      this.showModeratorBoard = this.roles.includes('ROLE_MODERATOR');
+
+      this.username = user.username;
+    }
+
+    this.eventBusSub = this.eventBusService.on('logout', () => {
+      this.logout();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.eventBusSub) this.eventBusSub.unsubscribe();
+  }
+
+  logout(): void {
+    this.storageService.signOut();
+
+    this.isLoggedIn = false;
+    this.roles = [];
+    this.showAdminBoard = false;
+    this.showModeratorBoard = false;
+  }
 }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { StorageService } from '../_services/storage.service';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+export class LoginComponent implements OnInit {
+  form: any = {
+    aliasName: null,
+    passWord: null,
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
+
+  constructor(
+    private authService: AuthService,
+    private storageService: StorageService
+  ) {}
+
+  ngOnInit(): void {
+    if (this.storageService.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.storageService.getUser().roles;
+    }
+  }
+
+  onSubmit(): void {
+    const { aliasName, passWord } = this.form;
+    console.log({ aliasName, passWord });
+    this.authService.login(aliasName, passWord).subscribe({
+      next: (data) => {
+        this.storageService.saveToken(data.accessToken);
+        this.storageService.saveRefreshToken(data.refreshToken);
+        this.storageService.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.storageService.getUser().roles;
+        this.reloadPage();
+      },
+      error: (err) => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      },
+    });
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
+}
