255018142198c2b03ba480bcc00c8295d5e92309
Antony-Lucas <lawokahama@gmail.com>
2024-04-16 22:45:08 -0300

    chart filter updated

--- src/app/DashView/screens/chart-screen/chart-screen.component.ts
+++ src/app/DashView/screens/chart-screen/chart-screen.component.ts
@@ -7,26 +7,27 @@ import { SkeletonModule } from 'primeng/skeleton';
 import { StorageService } from '../../../services/service/user/storage.service';
 import { HttpHeaders } from '@angular/common/http';
 import { ChartsService } from '../../../services/service/charts/charts.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chart-screen',
   standalone: true,
-  imports: [CommonModule, SkeletonModule, HighchartsChartModule],
+  imports: [CommonModule, SkeletonModule, HighchartsChartModule, FormsModule],
   templateUrl: './chart-screen.component.html',
   styleUrl: './chart-screen.component.css',
 })
-export class ChartScreenComponent implements AfterViewInit, OnInit {
+export class ChartScreenComponent implements AfterViewInit {
   loadingScreen!: boolean;
   Highcharts: typeof Highcharts = Highcharts;
 
   chartGroupsData: Highcharts.Options[] = [];
+  filters: any[] = [];
+  selectedFilters: any = {};
   chartObject: any;
+  checkedValues: any = {};
 
-  chartLeftTop_One!: Highcharts.Options;
-  chartLeftBottom_One!: Highcharts.Options;
-  chartLeftBottom_Two!: Highcharts.Options;
-  chartRigth_One!: Highcharts.Options;
-  chartRigth_Two!: Highcharts.Options;
+  showModal: boolean = false;
+  isLoginLoading: boolean = false;
 
   constructor(
     private router: Router,
@@ -52,286 +53,131 @@ export class ChartScreenComponent implements AfterViewInit, OnInit {
   loadData(chartData: any) {
     this.chartObject = JSON.parse(localStorage.getItem('chartGroup') || 'null');
     this.chartGroupsData = [];
-    console.log('chartobjetc ', this.chartObject);
+    this.filters = [];
+
+    chartData.forEach((chart: any) => {
+      if (chart.chartGroup.id == this.chartObject) {
+        chart.filters.forEach((filter: any) => {
+          const existingFilter = this.filters.find(
+            (f) => f.column === filter.column[0]
+          );
+          if (existingFilter) {
+            if (!existingFilter.values.includes(filter.value[0])) {
+              existingFilter.values.push(filter.value[0]);
+            }
+          } else {
+            this.filters.push({
+              column: filter.column[0],
+              values: filter.value,
+              dataType: 'string', // Aqui você pode determinar o tipo de dados do filtro
+            });
+          }
+        });
+      }
+    });
 
     chartData.forEach((dataItem: any) => {
-      console.log('chartdata ', dataItem.chartGroup.id);
       if (dataItem.chartGroup.id == this.chartObject) {
-        console.log('chartdata ', dataItem);
-      }
-      /*this.chartObject.forEach((chartGroup: any) => {
-        if (dataItem.id === chartGroup.id) {
-          console.log(dataItem);
-          const seriesData: any[] = [];
+        const seriesData: any[] = [];
 
-          dataItem.xAxisColumns.forEach((xAxisColumn: any, index: number) => {
-            const yAxisColumn = dataItem.yAxisColumns[index];
-            const seriesName = yAxisColumn ? yAxisColumn.name[0] : '';
-            const seriesValues = xAxisColumn.data.map(
-              (value: any, i: number) => {
-                const yAxisData = dataItem.yAxisColumns[0].data;
-                const name = yAxisData ? yAxisData[i] : ''; // Se yAxisData estiver indefinido, definimos name como uma string vazia
-                return {
-                  name: name,
-                  y: parseFloat(value),
-                };
-              }
-            );
+        dataItem.xAxisColumns.forEach((xAxisColumn: any, index: number) => {
+          const yAxisColumn = dataItem.yAxisColumns[index];
+          const seriesName = yAxisColumn ? yAxisColumn.name[0] : '';
 
-            seriesData.push({
-              name: seriesName,
-              data: seriesValues,
-            });
+          const seriesValues = xAxisColumn.data.map((value: any, i: number) => {
+            const yValue = yAxisColumn ? parseFloat(yAxisColumn.data[i]) : null;
+
+            return {
+              name: value,
+              y: yValue,
+            };
           });
 
-          const chartConfig: Highcharts.Options = {
-            chart: {
-              type: dataItem.graphType,
-            },
-            title: {
-              text: dataItem.title,
-            },
-            xAxis: {
-              categories: dataItem.yAxisColumns[0].data,
-              title: {
-                text: dataItem.yAxisColumns[0].name[0],
-              },
-            },
-            yAxis: {
-              title: {
-                text: dataItem.xAxisColumns[0].name[0],
-              },
-            },
-            series: seriesData,
-            tooltip: {
-              shared: true,
-            },
-          };
+          seriesData.push({
+            name: seriesName,
+            data: seriesValues,
+          });
+        });
 
-          this.chartGroupsData.push(chartConfig);
-        }
-      });*/
+        const chartConfig: Highcharts.Options = {
+          chart: {
+            type: dataItem.graphType,
+          },
+          title: {
+            text: dataItem.title,
+            style: {
+              fontSize: '14px',
+            },
+          },
+          xAxis: {
+            categories: dataItem.xAxisColumns[0].data,
+            title: {
+              text: dataItem.xAxisColumns[0].name[0],
+            },
+          },
+          yAxis: {
+            title: {
+              text: dataItem.yAxisColumns[0].name[0],
+            },
+          },
+          series: seriesData,
+          tooltip: {
+            shared: true,
+          },
+        };
+
+        this.chartGroupsData.push(chartConfig);
+      }
     });
+  }
+
+  onCheckboxChange(column: string, value: string) {
+    if (this.checkedValues[column] === undefined) {
+      this.checkedValues[column] = [];
+    }
+
+    const index = this.checkedValues[column].indexOf(value);
+    if (index === -1) {
+      this.checkedValues[column].push(value);
+      console.log(`Checkbox marcado para ${value}`);
+    } else {
+      this.checkedValues[column].splice(index, 1);
+      console.log(`Checkbox desmarcado para ${value}`);
+    }
+    this.updateDropdownLabel(column);
+  }
+
+  updateDropdownLabel(column: string): void {
+    if (this.checkedValues[column] && this.checkedValues[column].length > 0) {
+      this.selectedFilters[column] = this.checkedValues[column].join(', ');
+      console.log('column: ', this.selectedFilters);
+    } else {
+      this.selectedFilters[column] = 'Todos';
+      console.log('column: ', this.selectedFilters);
+    }
   }
 
   backScreen() {
     this.router.navigate(['/content/main/chartgroup']);
   }
 
-  ngOnInit(): void {
-    this.getCharts();
+  openModal(): void {
+    this.showModal = true;
+  }
+
+  closeModal(): void {
+    this.showModal = false;
+  }
+
+  executeFilter() {
+    this.isLoginLoading = true;
+    setTimeout(() => {
+      this.isLoginLoading = false;
+    }, 2500);
+    alert('tanka');
   }
 
   ngAfterViewInit(): void {
-    /*this.chartLeftTop_One = {
-      title: {
-        text: 'Receita por período',
-        align: 'center',
-      },
-
-      xAxis: {
-        type: 'category',
-        labels: {
-          autoRotation: [-45, -90],
-          style: {
-            fontSize: '12px',
-            fontFamily: 'Verdana, sans-serif',
-          },
-        },
-      },
-      plotOptions: {
-        series: {
-          borderWidth: 0,
-          dataLabels: {
-            enabled: true,
-          },
-        },
-      },
-      yAxis: {
-        min: 0,
-        title: {
-          text: '',
-        },
-      },
-      legend: {
-        enabled: false,
-      },
-
-      series: [
-        {
-          type: 'column',
-          name: 'Total',
-          colors: ['#0040ff'],
-          colorByPoint: true,
-          groupPadding: 0,
-          data: [
-            ['09/23', 36541],
-            ['10/23', 25415],
-            ['11/23', 29584],
-            ['12/23', 32546],
-            ['01/24', 23698],
-            ['02/24', 26413],
-            ['03/24', 23564],
-          ],
-        },
-      ],
-    };
-
-    this.chartRigth_One = {
-      title: {
-        text: 'Top 10: Receita por setor',
-      },
-      xAxis: {
-        type: 'category',
-        labels: {
-          autoRotation: [-45, -90],
-          style: {
-            fontSize: '13px',
-            fontFamily: 'Verdana, sans-serif',
-          },
-        },
-      },
-      plotOptions: {
-        column: {
-          borderRadius: '10%',
-        },
-        series: {
-          borderWidth: 0,
-          dataLabels: {
-            enabled: true,
-            format: '{point.y}%',
-          },
-        },
-      },
-
-      legend: {
-        enabled: false,
-      },
-
-      series: [
-        {
-          name: 'Total',
-          type: 'bar',
-          colorByPoint: true,
-          groupPadding: 0,
-          data: [
-            ['Setor A', 7945],
-            ['Setor B', 6512],
-            ['Setor C', 6123],
-            ['Setor D', 5315],
-            ['Setor E', 3567],
-            ['Setor F', 3197],
-            ['Setor G', 2581],
-            ['Setor H', 2015],
-            ['Setor I', 1502],
-            ['Setor J', 658],
-          ],
-          colors: [
-            '#9b20d9',
-            '#9215ac',
-            '#861ec9',
-            '#7a17e6',
-            '#7010f9',
-            '#691af3',
-          ],
-        },
-      ],
-    };
-
-    this.chartLeftBottom_One = {
-      title: {
-        text: 'Receita por tipo de atendimento',
-      },
-
-      plotOptions: {
-        series: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: [
-            {
-              enabled: true,
-              format: '{point.y:.0f}m | {point.name}',
-              style: {
-                fontSize: '.6em',
-                textOutline: 'none',
-                opacity: 0.7,
-              },
-            },
-          ],
-        },
-      },
-      series: [
-        {
-          type: 'pie',
-          name: 'Atendimento',
-          data: [
-            {
-              name: 'Ambulatorial',
-              y: 150,
-            },
-            {
-              name: 'Externo',
-              y: 106,
-            },
-            {
-              name: 'Internado',
-              y: 1615,
-            },
-            {
-              name: 'Pronto Atendimento',
-              y: 101,
-            },
-          ],
-        },
-      ],
-    };
-
-    this.chartLeftBottom_Two = {
-      title: {
-        text: 'Receita por tipo de convênio',
-      },
-
-      plotOptions: {
-        series: {
-          allowPointSelect: true,
-          cursor: 'pointer',
-          dataLabels: [
-            {
-              enabled: true,
-              format: '{point.y:.0f}m | {point.name}',
-              style: {
-                fontSize: '.6em',
-                textOutline: 'none',
-                opacity: 0.7,
-              },
-            },
-          ],
-        },
-      },
-      series: [
-        {
-          type: 'pie',
-          name: 'Atendimento',
-          data: [
-            {
-              name: 'Auto gestão',
-              y: 150,
-            },
-            {
-              name: 'Convênio',
-              y: 106,
-            },
-            {
-              name: 'Particular',
-              y: 1615,
-            },
-          ],
-        },
-      ],
-    };
-
-    setTimeout(() => {
-      this.loadingScreen = !this.loadingScreen;
-    }, 1000);*/
+    this.getCharts();
   }
 }
