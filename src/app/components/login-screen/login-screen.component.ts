6d094396413e0462880afcc12a35ef64aa05a952
Alícia B Mendes <aliealie.am@gmail.com>
2024-04-24 09:23:41 -0300

    feat: input choice adm user

--- src/app/components/login-screen/login-screen.component.ts
+++ src/app/components/login-screen/login-screen.component.ts
@@ -26,6 +26,11 @@ import { StorageService } from '../../services/service/user/storage.service';
   styleUrl: './login-screen.component.css',
 })
 export class LoginScreenComponent implements OnInit {
+  isADM!: boolean;
+  isLoading: boolean = true;
+  isModal: boolean = true;
+  isLoginLoading: boolean = false;
+
   form: any = {
     username: null,
     password: null,
@@ -44,29 +49,61 @@ export class LoginScreenComponent implements OnInit {
     if (this.storageService.getToken()) {
       this.roles = this.storageService.getUser().roles;
     }
+
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000); // padrão: 3000
+  }
+
+  defaultOrAdm(isAdmin: boolean): void {
+    if (isAdmin) {
+      this.redirectUser(isAdmin);
+    } else {
+      this.redirectUser(isAdmin);
+    }
+  }
+
+  redirectUser(isAdmin: boolean): void {
+    if (isAdmin == true) {
+      setTimeout(() => {
+        this.routes.navigate(['admin']);
+        this.isLoginLoading = false;
+      }, 1000); // padrão: 2000
+    } else if (isAdmin == false) {
+      setTimeout(() => {
+        this.routes.navigate(['content']);
+        this.isLoginLoading = false;
+      }, 1000); // padrão: 2000
+    }
   }
 
   onSubmit(): void {
     const { username, password } = this.form;
-
+    this.isLoginLoading = true;
     this.authService.login(username, password).subscribe({
       next: (data) => {
         this.storageService.saveToken(data.token);
         this.storageService.saveRefreshToken(data.refreshToken);
         this.storageService.saveUser(data);
         this.roles = this.storageService.getUser().roles;
-
         if (this.roles.includes('ROLE_ADMIN')) {
-          this.routes.navigate(['admin']);
+          setTimeout(() => {
+            this.isLoginLoading = false;
+            this.isModal = false;
+          }, 1000); // padrão: 2000
         } else {
-          this.routes.navigate(['content']);
+          setTimeout(() => {
+            this.routes.navigate(['content']);
+            this.isLoginLoading = false;
+          }, 1000); // padrão: 2000
         }
       },
       error: (err) => {
+        this.isLoginLoading = false;
         this.messageService.add({
           severity: 'error',
           summary: 'Erro',
-          detail: 'Erro na autenticação. ',
+          detail: 'Erro na autenticação.',
         });
         throw err;
       },
